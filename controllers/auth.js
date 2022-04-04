60d3b552082464d0f0c349a7845aaa79e860c28b
RicardoVenegasDavila <venegas_wow_72@hotmail.com>
2022-04-04 00:36:54 -0500

    Encriptar la contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,59 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
 
 
-const crearUsuario = (req, res) => {
-
-
-
+const crearUsuario = async (req, res) => {
 
     const { email, name, password } = req.body;
-    return res.json({
-        ok: true,
-        mgs: 'crear un nuevo usuario'
-    })
+
+
+
+    try {
+
+        //Verificar el email
+
+        let usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'el usuario ya existe con ese email'
+            })
+        }
+
+        //crear usuario con el model
+
+        const dbUser = new Usuario(req.body);
+
+        //Hashear la contraseña (encriptar la contraseña)
+
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync(password,salt);
+        //Generar el JWT
+
+        //Crear usuario de base de datos
+
+        await dbUser.save();
+        //Generar la respuesta exitosa
+
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            ok: true,
+            mgs: 'por favor hable con el administrador'
+        })
+    }
+
+
+
+
 }
 
 const loginUsuario = (req, res) => {
